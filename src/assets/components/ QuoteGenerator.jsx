d917c395036ef9b64ce5c948954041217bf33d68
Julie Hoagland-Sorensen <143046484+JulieHoaglandSorensen@users.noreply.github.com>
2025-01-08 11:31:52 -0500

    commented out stretch

--- src/assets/components/ QuoteGenerator.jsx
+++ src/assets/components/ QuoteGenerator.jsx
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
 
-function QuoteGenerator() {
-  const [quote, setQuote] = useState("");
+// function QuoteGenerator() {
+//   const [quote, setQuote] = useState("");
 
-  useEffect(() => {
-    async function fetchQuote() {
-      try {
-        const response = await axios.get("https://api.quotable.io/random", {
-          params: { tags: "reading" },
-        });
-        setQuote(response.data.content);
-      } catch (error) {
-        setQuote("“Books are a uniquely portable magic.” – Stephen King");
-      }
-    }
-    fetchQuote();
-  }, []);
+//   useEffect(() => {
+//     async function fetchQuote() {
+//       try {
+//         const response = await axios.get("https://api.quotable.io/random", {
+//           params: { tags: "reading" },
+//         });
+//         setQuote(response.data.content);
+//       } catch (error) {
+//         setQuote("“Books are a uniquely portable magic.” – Stephen King");
+//       }
+//     }
+//     fetchQuote();
+//   }, []);
 
-  return <div className="auth-subtitle">{quote}</div>;
-}
+//   return <div className="auth-subtitle">{quote}</div>;
+// }
 
-export default QuoteGenerator;
+// export default QuoteGenerator;
