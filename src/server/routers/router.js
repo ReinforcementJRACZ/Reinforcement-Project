f7082255ab2e74b325e690b4ac24dc1943a5fec1
ViaLeeny <Ayleen.nazario@gmail.com>
2025-01-07 21:11:43 -0500

    Create books route and expose for frontend to use

--- src/server/routers/router.js
+++ src/server/routers/router.js
@@ -6,3 +6,7 @@ export const router = express.Router();
 router.post('/populate-books-table', booksController.populateBooksTable, (req, res) => {
   res.status(200).send('Books populated in the database successfully');
 })
+
+router.get('/books', booksController.getBooks, (req, res) => {
+  res.status(200).json(req.books);
+})
